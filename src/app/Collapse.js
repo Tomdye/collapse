fd3ba44d3d240a3248a3d08d56e633a7cf156474
Tom Dye <github@ursaminor.co.uk>
2012-09-05 22:15:19 +0100

    reshuffle!!

--- src/app/Collapse.js
+++ src/app/Collapse.js
@@ -1,14 +1,15 @@
 define ([	"dojo/_base/declare",
 			"dijit/_WidgetBase",
     		"dijit/_TemplatedMixin",
-    		"dojo/text!./templates/collapse.html"
+    		"dojo/text!./templates/Collapse.html"
 ], function(declare, _WidgetBase, _TemplatedMixin, template) {
-	return declare("collapse", [_WidgetBase, _TemplatedMixin], {
+	return declare("Collapse", [_WidgetBase, _TemplatedMixin], {
 		
 		templateString: template,
 		
 		postCreate : function () {
 			this.inherited(arguments);
+			console.log("Collapse Started!");
 		}
 
 	})	
